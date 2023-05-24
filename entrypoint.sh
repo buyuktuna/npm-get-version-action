7c76a246e91cdcd0ce1388eab777114da32b0145
buyuktuna <buyuktuna@users.noreply.github.com>
2023-05-24 11:45:00 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 PACKAGE_JSON_PATH="${1-.}"
 echo "Reading package.json from ${PACKAGE_JSON_PATH}/package.json"
 PACKAGE_VERSION=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.version' | tr -d '"')
+PACKAGE_BUILD=$(cat ${PACKAGE_JSON_PATH}/package.json | jq '.build' | tr -d '"')
 
-echo "current-version=${PACKAGE_VERSION}" >> $GITHUB_OUTPUT
+echo "current-version=${PACKAGE_VERSION}.${PACKAGE_BUILD}" >> $GITHUB_OUTPUT
